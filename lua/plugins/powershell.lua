2889728148008e149cf9e33a8a1f736b6f71e13e
R. L. Vandaveer <rlvandaveer@heliar.io>
2025-11-10 21:19:56 -0500

    Add powershell.nvim

---
+++ lua/plugins/powershell.lua
@@ -0,0 +1,17 @@
+return {
+  "TheLeoP/powershell.nvim",
+  dependencies = {
+    "mfussenegger/nvim-dap", -- Required for debugging
+  },
+  ft = "ps1", -- Only load for PowerShell files
+  opts = {
+    bundle_path = vim.fn.stdpath("data") .. "/mason/packages/powershell-editor-services",
+  },
+  config = function(_, opts)
+    require("powershell").setup(opts)
+    -- Optional: Add keybinding for code evaluation
+    vim.keymap.set({ "n", "x" }, "<leader>E", function()
+      require("powershell").eval()
+    end, { desc = "Eval PowerShell code", ft = "ps1" })
+  end,
+}
