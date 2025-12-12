068ab6ed2a34515ae3ef0e40af2dc9f8326afec0
R. L. Vandaveer <rlvandaveer@heliar.io>
2025-12-12 09:29:17 -0500

    Improve hidden file handling in explorer

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,25 +1,20 @@
 -- ~/.config/nvim/lua/plugins/snacks.lua
 return {
   "folke/snacks.nvim",
-  opts = {
-    explorer = {
-      filters = {
-        git_ignored = false, -- Show git-ignored files
-        custom = { "^\\.git$" }, -- Hide only .git directory itself
-      },
-      ---@type snacks.explorer.Config.icons
-      icons = {
-        git_status = {
-          ignored = "◌", -- Icon for ignored files (or use "" to hide icon)
-        },
-      },
-      styles = {
-        -- Style for git-ignored files
-        git_ignored = {
-          fg = "comment", -- Dim color (uses your theme's comment color)
-          italic = true, -- Optional: make them italic
-        },
-      },
-    },
-  },
+  opts = function(_, opts)
+    opts.explorer = opts.explorer or {}
+    opts.explorer.filters = opts.explorer.filters or {}
+
+    opts.explorer.filters.git_ignored = false -- Show git-ignored files by default
+    opts.explorer.filters.custom = { "^%.git$", "^%.git/" } -- Hide .git directory
+
+    -- Optional: Add visual styling for ignored files
+    opts.explorer.styles = opts.explorer.styles or {}
+    opts.explorer.styles.git_ignored = {
+      fg = "comment",
+      italic = true,
+    }
+
+    return opts
+  end,
 }
