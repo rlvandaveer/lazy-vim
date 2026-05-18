0bb8b3fff829e7989c666ebaa187f1477c77af1a
R. L. Vandaveer <rlvandaveer@heliar.io>
2026-05-18 17:27:41 -0400

    Add more colorschemes and set default to srcery
    Add themes
    - cyberdream
    - darkearth.nvim
    - everforest
    - naysayer
    - gruvbox
    - srcery
    Update plugins

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,4 +1,20 @@
 return {
+  { "morhetz/gruvbox" },
   { "ptdewey/darkearth-nvim" },
   { "sainnhe/everforest" },
+  {
+    "RostislavArts/naysayer.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "srcery-colors/srcery-vim",
+    lazy = false,
+    priority = 1000,
+  },
 }
